5b04593cf879bf861f32ae4cb035a0ead4d7ca3a
unknown <arthur.lolico@gmail.com>
2022-03-11 10:28:22 -0300

    feat: add sum function for testing

---
+++ test.js
@@ -0,0 +1,5 @@
+const soma = require('./soma');
+
+test('adicionar 10 + 10 é igual a 20', () => {
+    expect(soma(10,10)).toEqual(20);
+  });
--- test.ts
+++
@@ -1,3 +0,0 @@
-test('my first test', () => {
-    expect(1).toEqual(1);
-  })
