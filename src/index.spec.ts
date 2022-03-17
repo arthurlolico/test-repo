7a4c994fb2e0cafb7ef648f72ec917e7c7a93d02
unknown <arthur.lolico@gmail.com>
2022-03-16 21:46:21 -0300

    feat: DynamoDB with Jest fully working

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,39 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import {
-  DynamoDBDocument,
-  GetCommand,
-  PutCommand,
-} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocument, GetCommand, PutCommand} from '@aws-sdk/lib-dynamodb';
 import DynamoDbLocal from 'dynamodb-local';
 
 let child: any;
 
 const port = 8000;
 
+jest.setTimeout(30000);
+
 beforeAll(async () => {
   child = await DynamoDbLocal.launch(port, null, [], false, true);
+  const items = [...new Array(1000)].map((_, index) => {
+    const newIndex = index + 1000;
+    const hashKey = 'pk';
+    const rangeKey = 'sk';
+    const hashKeyValue = 'DynamoDBCursorBasedPagination';
+    const rangeKeyValue = `cursor-${newIndex}`;
+
+    return {
+      [hashKey]: hashKeyValue,
+      [rangeKey]: rangeKeyValue,
+      index: newIndex,
+      parity: newIndex & 1 ? 'ODD' : 'EVEN',
+    };
+
+  });
+  
+  await Promise.all(items.map(item => {
+    return documentClient.send(
+      new PutCommand({
+        TableName: 'DynamoDBCursorBasedPagination',
+        Item: item
+      })
+    );
+ }));
 });
 
 afterAll(() => {
@@ -21,11 +43,11 @@ afterAll(() => {
 const documentClient = DynamoDBDocument.from(
   new DynamoDB({
     endpoint: `http://127.0.0.1:${port}`,
-    region: 'eu-central-1',
+    region: 'local-env',
     tls: false,
     credentials: {
-      accessKeyId: 'foo',
-      secretAccessKey: 'bar',
+      accessKeyId: 'fakeMyKeyId',
+      secretAccessKey: 'fakeSecretAccessKey',
     },
   }),
   {
@@ -37,16 +59,15 @@ const documentClient = DynamoDBDocument.from(
 );
 
 it('should insert item into table', async () => {
-  await documentClient.send(
-    new PutCommand({ TableName: 'files', Item: { id: '1', hello: 'world' } })
-  );
 
   const { Item } = await documentClient.send(
-    new GetCommand({ TableName: 'files', Key: { id: '1' } })
+    new GetCommand({ TableName: 'DynamoDBCursorBasedPagination', Key: { pk: 'DynamoDBCursorBasedPagination' , sk: 'cursor-1000'} })
   );
 
   expect(Item).toEqual({
-    id: '1',
-    hello: 'world',
+    pk: 'DynamoDBCursorBasedPagination',
+    sk: 'cursor-1000',
+    index: 1000,
+    parity: 'EVEN'
   });
 });
