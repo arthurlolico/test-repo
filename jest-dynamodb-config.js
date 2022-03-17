f2581234a21915f47b9356116c93dd832eb38e55
arthurlolico <arthur.lolico@gmail.com>
2022-03-17 08:18:55 -0300

    feat: DynamoDB with Jest fully working (#3)

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   tables: [
     {
-      TableName: `files`,
-      KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
-      AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+      TableName: `DynamoDBCursorBasedPagination`,
+      KeySchema: [{ AttributeName: 'pk', KeyType: 'HASH' }, { AttributeName: 'sk', KeyType: 'RANGE' }],
+      AttributeDefinitions: [{ AttributeName: 'pk', AttributeType: 'S' }, { AttributeName: 'sk', AttributeType: 'S' }],
       ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
     },
   ],
